2e9075f9fa7e4c7286811075f99fb715573aea9e
Guilherme-Vecchi <guivecchi12@gmail.com>
2022-01-06 10:54:02 -0700

    changes to login look

--- src/components/Login.js
+++ src/components/Login.js
@@ -119,49 +119,51 @@ const Login = () => {
   return (
     <div className="login">
         <form onSubmit={formSubmit} className="form">
-            <h1>LOG IN</h1>
-            {errors.message.length > 0 ? <div className="errorMessage">{errors.message}</div> : null}
-            <div className="userInfo">
-                <label htmlFor="username">
-                    Username
-                    <input
-                        type="text"
-                        name="username"
-                        onChange={handleChange}
-                        value={formState.username}
-                        label="Username"
-                        errors={errors}
-                    />
-                    {errors.username.length !== 0 && (
-                        <p className="error">{errors.username}</p>
-                    )}
-                </label>
-                <label htmlFor="password">
-                    Password
-                    <input
-                        type="password"
-                        name="password"
-                        onChange={handleChange}
-                        value={formState.password}
-                        label="Password"
-                        errors={errors}
-                    />
-                    {errors.password.length !== 0 && (
-                        <p className="error">{errors.password}</p>
-                    )}
-                </label>
-                <label className="terms" htmlFor="terms">
-                    <input name="terms" type="checkbox" onChange={handleChange}/>
-                    <Popup 
-                      trigger={<button> Terms of Service </button>} 
-                      modal
-                      closeOnDocumentClick
-                    >
-                      <div>These products are for personal enjoyment and not for resale</div>
-                    </Popup> 
-                </label>
+          <h1>LOG IN</h1>
+          {errors.message.length > 0 ? <div className="errorMessage">{errors.message}</div> : null}
+          <div className="userInfo">
+            <div className="myFlex">
+              <label htmlFor="username" className="label">
+                Username
+              </label>
+              <input
+                placeholder="Enter Username"
+                type="text"
+                name="username"
+                onChange={handleChange}
+                value={formState.username}
+                label="Username"
+                errors={errors}
+              />
+            </div> 
 
-                <button disabled={buttonDisabled}>SUBMIT</button>
+            <div className="myFlex"> 
+              <label htmlFor="password" className="label">
+                Password
+              </label>
+              <input
+                placeholder="Enter Password"
+                type="password"
+                name="password"
+                onChange={handleChange}
+                value={formState.password}
+                label="Password"
+                errors={errors}
+              />
+            </div>
+                
+            <label className="terms" htmlFor="terms">
+                <input name="terms" type="checkbox" onChange={handleChange}/>
+                <Popup 
+                  trigger={<button> Terms of Service </button>} 
+                  modal
+                  closeOnDocumentClick
+                >
+                  <div>These products are for personal enjoyment and not for resale</div>
+                </Popup> 
+            </label>
+
+            <button disabled={buttonDisabled} className="submit">SUBMIT</button>
             </div>  
         </form>
     </div>
